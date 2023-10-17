216e23d487ce128c5ca0a4f1ca53958ef16d2ded
Daniel Leroux <leroux.daniel@googlemail.com>
2023-10-17 19:57:59 +0200

    add postinstall

--- scripts/generate-examples.ts
+++ scripts/generate-examples.ts
@@ -1,32 +1,32 @@
 /*
  * COPYRIGHT (c) Siemens AG 2018-2023 ALL RIGHTS RESERVED.
  */
-import fs from 'fs-extra';
-import { Listr } from 'listr2';
-import path from 'path';
-import { rimraf } from 'rimraf';
-import { Readable } from 'stream';
-import { finished, pipeline } from 'stream/promises';
-import yauzl from 'yauzl-promise';
+import fs from "fs-extra";
+import { Listr } from "listr2";
+import path from "path";
+import { rimraf } from "rimraf";
+import { Readable } from "stream";
+import { finished, pipeline } from "stream/promises";
+import yauzl from "yauzl-promise";
 
-const rootPath = path.join(__dirname, '../');
+const rootPath = path.join(__dirname, "../");
 
-const tempWorkingDirPath = path.join(rootPath, '.temp-examples');
-const zipOutputPath = path.join(rootPath, '.temp-examples', 'output');
+const tempWorkingDirPath = path.join(rootPath, ".temp-examples");
+const zipOutputPath = path.join(rootPath, ".temp-examples", "output");
 
 (async () => {
   // CommonJS Workaround
-  const { execaCommand } = await import('execa');
+  const { execaCommand } = await import("execa");
 
   const branch = process.env.IX_DOCS_BRANCH;
   const file = path.join(tempWorkingDirPath, `ix-${branch}.zip`);
 
   const unpackFilter = [
-    'vue-test-app',
-    'react-test-app',
-    'angular-test-app',
-    'html-test-app',
-    path.join('core', 'component-doc.json'),
+    "vue-test-app",
+    "react-test-app",
+    "angular-test-app",
+    "html-test-app",
+    path.join("core", "component-doc.json"),
   ];
 
   const tasks = new Listr(
@@ -39,9 +39,9 @@ const zipOutputPath = path.join(rootPath, '.temp-examples', 'output');
           await fs.ensureDir(zipOutputPath);
 
           const response = await fetch(
-            'https://github.com/siemens/ix/archive/refs/heads/main.zip'
+            "https://github.com/siemens/ix/archive/refs/heads/main.zip"
           );
-          const fileStream = fs.createWriteStream(file, { flags: 'wx' });
+          const fileStream = fs.createWriteStream(file, { flags: "wx" });
           await finished(Readable.fromWeb(response.body!).pipe(fileStream));
         },
       },
@@ -61,7 +61,7 @@ const zipOutputPath = path.join(rootPath, '.temp-examples', 'output');
                   path.join(zipOutputPath, path.dirname(entry.filename))
                 );
 
-                if (!entry.filename.endsWith('/')) {
+                if (!entry.filename.endsWith("/")) {
                   const readStream = await entry.openReadStream();
                   const writeStream = fs.createWriteStream(
                     path.join(zipOutputPath, entry.filename)
@@ -76,18 +76,18 @@ const zipOutputPath = path.join(rootPath, '.temp-examples', 'output');
         },
       },
       {
-        title: 'Build Demos',
+        title: "Build Demos",
         task: async () => {
           const cwd = path.join(
             zipOutputPath,
             `ix-${branch}`,
-            'packages',
-            'html-test-app'
+            "packages",
+            "html-test-app"
           );
-          await execaCommand('pnpm install', {
+          await execaCommand("pnpm install", {
             cwd,
           });
-          await execaCommand('pnpm build', {
+          await execaCommand("pnpm build", {
             cwd,
           });
         },
